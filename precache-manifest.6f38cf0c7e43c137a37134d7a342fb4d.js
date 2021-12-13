93ec8f1270992e16db6017f1b06431da4f1a1932
Shruti123 <shrutib00@gmail.com>
2021-12-13 19:45:40 +0530

    Updates

---
+++ precache-manifest.6f38cf0c7e43c137a37134d7a342fb4d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "491f208187e39c4490b587115f55986a",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "1d62fa317b24ab28b676",
+    "url": "/my-app/static/css/main.f418fd8b.chunk.css"
+  },
+  {
+    "revision": "30b3993fe6055ad5ed67",
+    "url": "/my-app/static/js/2.b22862e3.chunk.js"
+  },
+  {
+    "revision": "4b7448a9c505669f244a7eac984d25a6",
+    "url": "/my-app/static/js/2.b22862e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22691b1ebb5c0285fcce",
+    "url": "/my-app/static/js/3.8bd91e5b.chunk.js"
+  },
+  {
+    "revision": "1d62fa317b24ab28b676",
+    "url": "/my-app/static/js/main.c7d75924.chunk.js"
+  },
+  {
+    "revision": "cd7c779e5c860290be08",
+    "url": "/my-app/static/js/runtime-main.8ac92a85.js"
+  }
+]);
